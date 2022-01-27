27a6c1519973edc74ca12889736ef49c35a7c5a9
Oreoluwa91 <emmanueloreoluwa91@gmail.com>
2022-01-27 17:29:47 +0100

    fifth commit

--- src/components/mrken/body.jsx
+++ src/components/mrken/body.jsx
@@ -22,7 +22,7 @@ class Body extends React.Component {
                             
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img src="Images/2040033.png" className="d-block w-100" alt=".." />
+                                        <img src="Images/ore.png" className="d-block w-100" alt=".." />
                                             <div class="carousel-caption d-none d-md-block">
                                                 <h5>First slide label</h5>
                                                 <p>Some representative placeholder content for the first slide.</p>
@@ -36,7 +36,7 @@ class Body extends React.Component {
                                                 </div>
                                     </div>
                                     <div class="carousel-item">
-                                                <img src="Images/istockphoto-882193344-1024x1024.jpg" className="d-block w-100" alt="..."/>
+                                                <img src="Images/2040033.pnggit" className="d-block w-100" alt="..."/>
                                                     <div class="carousel-caption d-none d-md-block">
                                                         <h5>Third slide label</h5>
                                                         <p>Some representative placeholder content for the third slide.</p>
--- src/components/mrken/navbar.jsx
+++ src/components/mrken/navbar.jsx
@@ -64,7 +64,7 @@ class Navbar extends React.Component {
     render() {
         return (
                 <nav className="navbar navbar-expand-lg navbar-light bg-info">
-                    <NavLink className="navbar-brand" to="/">FUCK Quadri</NavLink>
+                    <NavLink className="navbar-brand" to="/">My babyy</NavLink>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
